f02fd15d110c9e16e9abeb84f8ed913281cb45df
tabby35 <tabby35@users.noreply.github.com>
2020-06-08 13:43:44 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -3,31 +3,30 @@
 
 int main(void)
 {
-int height;
-do
+ int height;
+ do
  {
   //get height from user
- //printf();
- height=get_int("give me the height:\n");
+  //printf();
+  height=get_int("give me the height:\n");
  }
  while(height<1 || height>8);
 
-
  int row;
-{
- for(row=0;row<height;row++)
  {
+  for(row=0;row<height;row++)
+  {
    for(int space=height-1;space>row;space--)
-      {
-       //space
-       printf(" ");
-      }
-    for(int hashes=1;hashes<=row+1;hashes++)
-      {
-       //hashes
-         printf("#");
-      }
- printf("\n");
+    {
+     //space
+     printf(" ");
+    }
+   for(int hashes=1;hashes<=row+1;hashes++)
+    {
+     //hashes
+     printf("#");
+    }
+  printf("\n");
+  }
  }
-}
 }
