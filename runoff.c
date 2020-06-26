937bba052fe88d1d5d0bc2e60a2d5c6c62854c53
tabby35 <tabby35@users.noreply.github.com>
2020-06-26 17:47:29 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -211,7 +211,7 @@ bool is_tie(int min)
     for(int i=0;i<candidate_count;i++)
     {
         // TODO
-        if(candidates[i].votes>=min && candidates[i].eliminated==false)
+        if(candidates[i].votes>=min && candidates[i].eliminated==true)
         {
           return false;
         }
